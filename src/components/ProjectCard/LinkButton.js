9660b337b0b88bc8be314eb27a921b25ef47b493
lasnab <hrithikbansal70@gmail.com>
2021-07-25 21:06:46 -0400

    fixed software page links

--- src/components/ProjectCard/LinkButton.js
+++ src/components/ProjectCard/LinkButton.js
@@ -4,7 +4,7 @@ function LinkButton({ link = { name: '', url: '' } }) {
   const { name, url } = link;
   return (
     <a
-      className="no-underline black ba b-blue br3 ph2 pv0 mh2 hover-blue"
+      className="grow no-underline black ba b-blue br3 ph2 pv0 mh2 hover-blue"
       href={url}
     >
       {name}
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,7 +3,7 @@ import LinkButton from './LinkButton';
 
 function ProjectCard({ name, image, links, description }) {
   return (
-    <div className="grow ba b--gray br4 w-auto w5-ns w-40-m w-20-l mv3 mh3">
+    <div className="ba b--gray br4 w-auto w5-ns w-40-m w-20-l mv3 mh3">
       <img className="br--top br4 w-100" src={image} alt="project" />
       <div className="pl3 pr2 pv2">
         <div className="no-underline f3 tracked-tight mt4 gray hover-black">
